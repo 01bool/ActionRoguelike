af3694aabd61136bc18316ea3ca45d79dccff645
Neputune <1308667004@qq.com>
2022-07-07 01:35:38 +0800

    Add Magic Attack
    -Add Magic Attack
    -Add Jump Action
    -Update Animation BluePrints

--- Source/ActionRoguelike/Public/SCharacter.h
+++ Source/ActionRoguelike/Public/SCharacter.h
@@ -14,6 +14,11 @@ class ACTIONROGUELIKE_API ASCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
+protected:
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<AActor> ProjectileClass;
+
 public:
 	// Sets default values for this character's properties
 	ASCharacter();
@@ -31,6 +36,14 @@ protected:
 	virtual void BeginPlay() override;
 
 	void MoveForward(float Value);
+
+	void MoveRight(float Value);
+
+	void PrimaryAttack();
+
+	void JumpStart();
+
+	void JumpEnd();
 
 public:	
 	// Called every frame
